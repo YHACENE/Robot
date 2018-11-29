33c10495867d806597d57aec7cdfe2e039f13998
Odlareg93 <g.labamendes@gmail.com>
2018-11-29 23:43:46 +0100

    server web, project opencv+gst

---
+++ project_opencv.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+#import gst
+#import pygst
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst 
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import numpy
+import time
+
+class VideoCamera():
+	def __init__(self, interval = 0.1):
+		self.camera = PiCamera()
+		self.rawCapture = None
+		self.classifier = None
+		self.interval = interval
+
+	def prep_cam(self, dimL, diml, rate):
+		self.camera.resolution  = (dimL, diml)
+		self.camera.framerate = rate
+		self.rawCapture = PiRGBArray(self.camera, size=(dimL, diml))
+		self.classifier = cv2.CascadeClassifier("/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml") 
+		time.sleep(0.1)
+
+	def get_faces(self, frame):
+		minisize = (frame.shape[1]/4, frame.shape[0]/4)
+		miniframe = cv2.resize(frame, minisize)
+		faces = self.classifier.detectMultiScale(miniframe)
+		return faces
+
+	def camera_show(self):
+		#fourcc = cv2.VideoWriter_fourcc(*'MPG4')
+		fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+		#out = cv2.VideoWriter('test.avi', fourcc, 20.0, (640, 480))
+		out = cv2.VideoWriter('appsrc ! queue ! h264parse ! videoconvert ! rtph264pay config-interfal=1 pt=96 ! gdppay ! tcpserversink host=192.168.10.12 port= 5000', fourcc, 20.0,(640, 480))
+
+		for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
+			time.sleep(self.interval)
+			image = frame.array
+			faces = self.get_faces(image)
+			for f in faces:
+				x, y, w, h = [v*4 for v in f]
+				cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0))
+
+			cv2.imshow("Image", image)
+			out.write(image)
+			key = cv2.waitKey(1) & 0xFF
+			self.rawCapture.truncate(0)
+			if key == ord('q'):
+				break
+		out.release()
+		cv2.destroyAllWindows()
+
+if __name__=="__main__":
+	PC = VideoCamera()
+	PC.prep_cam(640, 480, 25) #1280, 720
+	PC.camera_show()
