b656b82f1d84f6b45d6962e912230d0c4c3d68df
Odlareg93 <g.labamendes@gmail.com>
2018-11-30 01:21:11 +0100

    test implémentation gstrtsp server

--- project_opencv.py
+++ project_opencv.py
@@ -7,12 +7,31 @@ import sys
 #import pygst
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gst 
+gi.require_version('GstRtspServer', '1.0') 
+from gi.repository import GObject, Gst, GstRtspServer
 import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy
 import time
+
+class MyFactory(GstRtspServer.RTSPMediaFactory):
+	def __init__(self):
+		GstRtspServer.RTSPMediaFactory.__init__(self)
+
+	def do_create_element(self, url):
+		spec =  """ appsrc ! queue ! h264parse ! videoconvert ! rtph264pay config-interfal=1 pt=96"""
+		return Gst.parse_launch(spec)
+
+class GstServer():
+	def __init__(self):
+		self.server = GstRtspServer.RTSPServer()
+		self.server.set_service("3002")
+		f = MyFactory()
+		f.set_shared(True)
+		m = self.server.get_mount_points()
+		m.add_factory("/capture", f)
+		self.server.attach(None)
 
 class VideoCamera():
 	def __init__(self, interval = 0.1):
@@ -36,9 +55,9 @@ class VideoCamera():
 
 	def camera_show(self):
 		#fourcc = cv2.VideoWriter_fourcc(*'MPG4')
-		fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+		#fourcc = cv2.cv.CV_FOURCC(*'MJPG')
 		#out = cv2.VideoWriter('test.avi', fourcc, 20.0, (640, 480))
-		out = cv2.VideoWriter('appsrc ! queue ! h264parse ! videoconvert ! rtph264pay config-interfal=1 pt=96 ! gdppay ! tcpserversink host=192.168.10.12 port= 5000', fourcc, 20.0,(640, 480))
+		#out = cv2.VideoWriter('appsrc ! queue ! h264parse ! videoconvert ! rtph264pay config-interfal=1 pt=96 ! gdppay ! tcpserversink host=192.168.10.12 port= 5000', fourcc, 20.0,(640, 480))
 
 		for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
 			time.sleep(self.interval)
@@ -48,16 +67,21 @@ class VideoCamera():
 				x, y, w, h = [v*4 for v in f]
 				cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0))
 
-			cv2.imshow("Image", image)
-			out.write(image)
+			#cv2.imshow("Image", image)
+			#out.write(image)
 			key = cv2.waitKey(1) & 0xFF
 			self.rawCapture.truncate(0)
 			if key == ord('q'):
 				break
-		out.release()
-		cv2.destroyAllWindows()
+		#out.release()
+		#cv2.destroyAllWindows()
 
 if __name__=="__main__":
 	PC = VideoCamera()
 	PC.prep_cam(640, 480, 25) #1280, 720
 	PC.camera_show()
+	loop = GObject.MainLoop()
+	GObject.threads_init()
+	Gst.init(None)
+	s = GstServer()
+	loop.run()
