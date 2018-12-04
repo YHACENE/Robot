93ec9bcfa0ee2fcff7dadaea6fcce30ba7ec735a
Odlareg93 <g.labamendes@gmail.com>
2018-12-04 19:28:54 +0100

    Server final

---
+++ pi_server_v2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#coding:utf-8
+
+from socket import *
+from time import sleep
+import threading
+
+def test():
+	print "Succefull"
+
+ctr_cmds = {
+	"forward" : test
+}
+
+HOST = ''
+PORT = 12000
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+class MyServer(threading.Thread):
+	def __init__(self, ip, port, clientsocket):
+		threading.Thread.__init__(self)
+		self.ip = ip
+		self.port = port
+		self.clientsocket = clientsocket
+		#print("[+] Nouveau thread pour %s %s %s" % (self.ip, self.port,getThreadId(), ))
+		
+	def run(self):
+		#print("Connexion de %s %s" % (self.ip, self.port, ))
+		cmd = ctr_cmds.get(self.clientsocket.recv(BUFSIZE), None)
+
+		try:
+			if cmd is not Nnone:
+				cmd()
+
+		except KeyboardInterrupt as e:
+			print("#ERROR: {}".format(e))
+
+tcpsock = socket(AF_INET, SOCK_STREAM)
+tcpsock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+tcpsock.bind(ADDR)
+
+if __name__ == "__main__":
+	while  True:
+		tcpsock.listen(10)
+		print( "En écoute...")
+		(clientsocket, (ip, port)) = tcpsock.accept()
+		newthread = MyServer(ip, port, clientsocket)
+		newthread.start()
