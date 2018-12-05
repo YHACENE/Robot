51923ca6b62ac7479d82bb0e71e9fbec8e31dfaf
toukfa <mm-tekfa@hotmail.com>
2018-12-05 15:51:57 +0100

    L298n

---
+++ l298n.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.output(7, True)
+time.sleep(1)
+GPIO.output(11, False)
+GPIO.output(13, True)
+time.sleep(1)
+GPIO.output(13, False)
+GPIO.output(15, True)
+time.sleep(1)
+GPIO.output(15, False)
+GPIO.cleanup()
