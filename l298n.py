8f34bfc5887bab60c39df126d1c771a1e8033187
Odlareg93 <g.labamendes@gmail.com>
2018-12-05 18:48:01 +0100

    stream picam on rstp flux v2

--- l298n.py
+++ l298n.py
@@ -4,14 +4,14 @@ Pilotage du composant l298n
 import RPi.GPIO as GPIO
 from time import sleep
 
-<<<<<<< HEAD
+
 def init_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
 
 def forward(tf):
     init_gpio()
@@ -35,19 +35,3 @@ print("Forward")
 forward(4)
 print("Backward")
 backward(4)
-=======
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW)
-
-GPIO.output(20, GPIO.HIGH)
-sleep(5)
-GPIO.output(20, GPIO.LOW)
-sleep(5)
-GPIO.output(26, GPIO.HIGH)
-sleep(5)
-GPIO.output(26, GPIO.LOW)
-GPIO.cleanup()
->>>>>>> ea100d364bf5e5ea2db0b1c108f3e4d9cc229537
