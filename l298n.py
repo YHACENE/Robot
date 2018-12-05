caecd9f1b420b7f884984cd843dc304b28f31bba
Hacene YOUNSI <hacene.younsi@hotmail.com>
2018-12-05 17:11:23 +0100

    fct forward backward test

--- l298n.py
+++ l298n.py
@@ -4,19 +4,33 @@ Pilotage du composant l298n
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setwarnings(False)
-GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
+def init_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.output(7, True)
-time.sleep(1)
-GPIO.output(11, False)
-GPIO.output(13, True)
-time.sleep(1)
-GPIO.output(13, False)
-GPIO.output(15, True)
-time.sleep(1)
-GPIO.output(15, False)
-GPIO.cleanup()
+def forward(tf):
+    init_gpio()
+    GPIO.output(17, False)
+    GPIO.output(22, True)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    sleep(tf)
+    GPIO.cleanup()
+
+def backward(tf):
+    init_gpio()
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    sleep(tf)
+    GPIO.cleanup()
+
+print("Forward")
+forward(4)
+print("Backward")
+backward(4)
