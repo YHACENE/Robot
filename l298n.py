d2465395c4d20c185245a9972294b7e4c54b29c9
Odlareg93 <g.labamendes@gmail.com>
2018-12-10 20:36:26 +0100

    control moteurs: add left and right fct

--- l298n.py
+++ l298n.py
@@ -6,6 +6,7 @@ from time import sleep
 
 
 def init_gpio():
+    print ("init")
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO.setup(17, GPIO.OUT)
@@ -14,24 +15,44 @@ def init_gpio():
     GPIO.setup(24, GPIO.OUT)
 
 def forward(tf):
+    print("forward")
     init_gpio()
-    GPIO.output(17, False)
-    GPIO.output(22, True)
-    GPIO.output(23, False)
-    GPIO.output(24, True)
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(22, GPIO.HIGH)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.HIGH)
     sleep(tf)
     GPIO.cleanup()
 
 def backward(tf):
     init_gpio()
-    GPIO.output(17, True)
-    GPIO.output(22, False)
-    GPIO.output(23, True)
-    GPIO.output(24, False)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)
+    GPIO.output(23, GPIO.HIGH)
+    GPIO.output(24, GPIO.LOW)
     sleep(tf)
     GPIO.cleanup()
 
-print("Forward")
-forward(4)
-print("Backward")
-backward(4)
+def left(tf):
+    init_gpio()
+    p = GPIO.PWM(24, 400)
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(22, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    sleep(tf)
+    GPIO.cleanup()
+    p.stop()
+
+def right(tf):
+    init_gpio()
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(22, GPIO.LOW)
+    GPIO.output(23, GPIO.LOW)
+    GPIO.output(24, GPIO.HIGH)
+    sleep(tf)
+    GPIO.cleanup()
+#print("Forward")
+#forward(4)
+#print("Backward")
+#backward(4)
